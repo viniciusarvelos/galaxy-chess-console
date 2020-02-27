c7d97d4287b25264d3e73b740c3b499b1ca85d43
Vinicius Arvelos <viniciusarvelos@gmail.com>
2020-02-27 12:49:51 -0300

    project created

---
+++ chess-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chess_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
